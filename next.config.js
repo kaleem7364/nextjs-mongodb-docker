ddbd6583a5e28f7cb39de9c39c66ede80eefa44c
kaleem7364 <mkoigxxsar@gmail.com>
2025-06-22 03:17:39 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+}
+
+module.exports = nextConfig
