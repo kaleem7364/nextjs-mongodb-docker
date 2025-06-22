73dd87ebfdc27a2092f384733ecc93aa787d7c2e
kaleem7364 <mkoigxxsar@gmail.com>
2025-06-22 03:11:30 +0300

    Create seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      { name: 'John Doe', email: 'john@example.com' },
+      { name: 'Jane Smith', email: 'jane@example.com' },
+    ],
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
